13d79409dd3648411ad3708a90d9fb96bb2c9605
SAGNIK SANYAL <sagniksanyal1502@gmail.com>
2024-10-04 21:32:06 +0530

    Refactor app_test.dart and remove unused code

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mynewz/app/app.dart';
-import 'package:mynewz/features/feed/counter.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (WidgetTester tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
     });
   });
 }
